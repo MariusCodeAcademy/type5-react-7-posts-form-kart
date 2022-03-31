1566fc6b0bd2d3dc4ea15e25ce0b81ed2769e3fd
Marius <marius@example.com>
2022-03-31 16:59:09 +0300

    uzd

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# react 4
+# react 7
+
+1. PostItem pridet darkMode input checkbox, kai pazymetas(checked) tas postas tampa juodas su baltu tekstu (jei per sunku checkbox daryti button)
+2. PostItem prideti delete mygtuka kuris istrina post
+3. Header komponente atvaizduoti kiek posts turim
+4. Sukurti komponenta CategoryList, ideti ji virs PostsList
+5. Sukurti komponenta CategoryItem. ideti ji i CategoryList.
+6. Sukurti App.js state categoriesArr = [art, music, tech]
+7. Paduoti categoriesArr i CategoryList.
+8. CategoryList generuoti CategoryItem is gauto categoriesArr.
+9. CategoryItem atvaizduoti graziai su css kaip filtro mygtukus.
+10. Sukurti komponenta NewCategoryForm. forma su vienu input el ir submit btn
+11. Ideti NewCategoryForm i FormList, po NewPostForm
+12. NewCategoryForm inputa susieti su state reiksme
+13. NewCategoryForm submitinant forma paimti reikme ir nusiusti i App.js
+14. App.js gavus reiksme is NewCategoryForm prideti ja i categoriesArr state
+15. Pakeitimai turetu atsipindeti CategoryList sarase.
--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import Header from './components/Header';
 import PostList from './components/PostList';
 // importuoti posts masyva is data/posts.js
 import initPosts from './data/posts';
+import { useState } from 'react';
 // peruodi ji i PostList componenta
 
 function App() {
-  // console.log('initPosts ===', initPosts);
+  const [postsArr, setPostsArr] = useState(initPosts);
+
   function newPostHandler(newPostObj) {
     console.log('newPostHandler in App.js', newPostObj);
+    // prideti id propertie prie newPostObj
+    // id: Math.random()
+    newPostObj.id = Math.random();
+    // nustatyti postsArr su setPostsArr() paduodant esama postsArr + newPostObj
+    // array.concat(newobj), array.slice() ...arrray
+    console.log('newPostObj ===', newPostObj);
+    // setPostsArr((prevState) => [...prevState, newPostObj]);
+    setPostsArr((prevState) => prevState.concat(newPostObj));
   }
   return (
     <div className='App'>
       <Header />
-      <h1>App</h1>
+      <h1>Posts App</h1>
       <main className='content'>
-        <PostList items={initPosts} />
+        <PostList items={postsArr} />
         <FormList onNewPost={newPostHandler} />
       </main>
     </div>
