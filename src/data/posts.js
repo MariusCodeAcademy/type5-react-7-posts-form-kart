12c0d4f31bef482d90bf8470c41b0068a46feb6c
Marius <marius@example.com>
2022-03-31 14:00:52 +0300

    data

---
+++ src/data/posts.js
@@ -0,0 +1,20 @@
+const posts = [
+  {
+    id: 1,
+    title: 'Post 1',
+    author: 'James',
+    body: 'This is body of Post 1',
+  },
+  {
+    id: 2,
+    title: 'Post 2',
+    author: 'Mike',
+    body: 'This is body of Post 2',
+  },
+  {
+    id: 3,
+    title: 'Post 3',
+    author: 'Jane',
+    body: 'This is body of Post 3',
+  },
+];
