bab55c15c5bca5f8bbfe5299a1b5003a607572a9
Marius <marius@example.com>
2022-03-31 14:00:46 +0300

    data posts.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+// importuoti posts masyva is data/posts.js
+// peruodi ji i PostList componenta
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <h1>App</h1>
     </div>
   );
