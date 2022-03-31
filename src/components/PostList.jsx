b69e858da27d45db8ae9596258930889525b9dc4
Marius <marius@example.com>
2022-03-31 14:52:21 +0300

    addDash('James') uzd

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,6 +3,7 @@ import PostItem from './PostItem';
 export default function PostList(props) {
   console.log(props.items);
   // generuoti  <PostItem /> is props.items ir paduoti jiems title, body, author
+  // <PostItem /> turi tilpti 3 i viena eilute ir buti su css graziai apipavidalinti(border, shadow, pading ir pan)
   return (
     <div>
       I am a PostList
---
+++ src/helper/helper.js
@@ -0,0 +1,4 @@
+// helper.js
+// sukurti funkcija, kuri paima stringa ir grazina stringa apjuosta su __
+// addDash('James') grazina '__James__'
+// panaudoti funkcija PostItem komponente, autoriaus atvaizdavimui
